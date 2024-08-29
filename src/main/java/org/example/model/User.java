aea62c05773c1b87f9ae581bd903c656e6d8fc66
Shushanna Grigoryan <shushannagrigoryan7@gmail.com>
2024-08-29 16:32:44 +0400

    Created User class.

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,12 @@
+package org.example.model;
+
+public class User {
+    protected Long userId;
+    protected  String firstName;
+    protected String lastName;
+    protected String username;
+    protected String password;
+    protected boolean isActive;
+
+
+}
