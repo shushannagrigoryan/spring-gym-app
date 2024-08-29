548af8c806d8004802195328ff553e94fa5251ae
Shushanna Grigoryan <shushannagrigoryan7@gmail.com>
2024-08-29 22:32:15 +0400

    Created a ValidatePassword class.

---
+++ src/main/java/org/example/ValidatePassword.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.regex.Pattern;
+
+public class ValidatePassword {
+    private static final String PATTERN = "^[a-zA-Z0-9@#$%^&+=!]{10}$";
+
+
+    public static boolean isValidPassword(String password){
+        if(password == null){
+            return false;
+        }
+        Pattern pattern = Pattern.compile(PATTERN);
+        return pattern.matcher(password).matches();
+    }
+}
