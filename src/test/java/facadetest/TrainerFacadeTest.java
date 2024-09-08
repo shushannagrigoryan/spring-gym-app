c8e8af4f0768b652ac353e9efe11e9ab9d937bf5
Shushanna Grigoryan <shushannagrigoryan7@gmail.com>
2024-09-08 21:32:22 +0400

    Unit tests for TrainerFacade  class.

--- src/test/java/facadetest/TrainerFacadeTest.java
+++ src/test/java/facadetest/TrainerFacadeTest.java
@@ -1,9 +1,9 @@
 package facadetest;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -31,74 +31,129 @@ public class TrainerFacadeTest {
 
     @Test
     public void testCreateTrainerSuccess() {
+        //given
         TrainerDto trainerDto = new TrainerDto();
         TrainerEntity trainerEntity = new TrainerEntity();
         when(trainerMapper.dtoToEntity(trainerDto)).thenReturn(trainerEntity);
+        doNothing().when(trainerService).createTrainer(trainerEntity);
 
+        //when
         trainerFacade.createTrainer(trainerDto);
 
-        verify(trainerService, times(1)).createTrainer(trainerEntity);
+        //then
+        verify(trainerService).createTrainer(trainerEntity);
     }
 
     @Test
     void testCreateTrainerInvalidPassword() {
+        //given
         TrainerDto trainerDto = new TrainerDto();
+        String password = "illegalPassword";
+        trainerDto.setPassword(password);
         TrainerEntity trainerEntity = new TrainerEntity();
         when(trainerMapper.dtoToEntity(trainerDto)).thenReturn(trainerEntity);
+        doThrow(new GymIllegalPasswordException(password))
+                .when(trainerService).createTrainer(trainerEntity);
 
-        doThrow(GymIllegalPasswordException.class).when(trainerService).createTrainer(trainerEntity);
+        //when
         trainerFacade.createTrainer(trainerDto);
 
-        verify(trainerService, times(1)).createTrainer(trainerEntity);
+        //then
+        GymIllegalPasswordException exception = assertThrows(GymIllegalPasswordException.class,
+                () -> trainerService.createTrainer(trainerEntity));
+        assertEquals("Illegal password: " + password, exception.getMessage());
     }
-
-    //    @Test
-    //    public void testGetTrainerByUsernameSuccess() {
-    //        String username = "JackSmith";
-    //        TrainerDto trainerDto = new TrainerDto();
-    //        when(trainerService.getTrainerByUsername(username)).thenReturn(trainerDto);
-    //
-    //        TrainerDto actualTrainerDto = trainerFacade.getTrainerByUsername(username);
-    //        verify(trainerService, times(1)).getTrainerByUsername(username);
-    //        assertEquals(trainerDto, actualTrainerDto);
-    //    }
-    //
-    //    @Test
-    //    public void testGetTrainerByUsernameInvalidUsername() {
-    //        String username = "invalidUsername";
-    //        when(trainerService.getTrainerByUsername(username)).thenThrow(new IllegalUsernameException(username));
-    //
-    //        assertThatThrownBy(() -> trainerFacade.getTrainerByUsername(username))
-    //                .isInstanceOf(IllegalUsernameException.class)
-    //                .hasMessageContaining("Illegal username: " + username);
-    //
-    //        verify(trainerService, times(1)).getTrainerByUsername(username);
-    //    }
 
     @Test
     public void testGetTrainerByIdSuccess() {
+        //given
         Long id = 1L;
         TrainerDto trainerDto = new TrainerDto();
-
         when(trainerService.getTrainerById(id)).thenReturn(trainerDto);
 
-        TrainerDto trainerDtoActual = trainerService.getTrainerById(id);
+        //when
+        trainerFacade.getTrainerById(id);
 
-        assertEquals(trainerDto, trainerDtoActual);
-        verify(trainerService, times(1)).getTrainerById(id);
+        //then
+        verify(trainerService).getTrainerById(id);
     }
 
     @Test
     public void testGetTrainerByIdFailure() {
+        //given
         Long id = 1L;
+        doThrow(new GymIllegalIdException("No trainer with id: " + id))
+                .when(trainerService).getTrainerById(id);
 
-        when(trainerService.getTrainerById(id))
-                .thenThrow(new GymIllegalIdException("No trainer with id: " + id));
-        assertThatThrownBy(() -> trainerService.getTrainerById(id))
-                .isInstanceOf(GymIllegalIdException.class).hasMessageContaining("No trainer with id: " + id);
-        verify(trainerService, times(1)).getTrainerById(id);
+        //when
+        trainerFacade.getTrainerById(id);
+
+        //then
+        verify(trainerService).getTrainerById(id);
+        GymIllegalIdException exception = assertThrows(GymIllegalIdException.class,
+                () -> trainerService.getTrainerById(id));
+        assertEquals("No trainer with id: " + id, exception.getMessage());
+    }
+
+
+
+
+    @Test
+    public void testUpdateTrainerByIdSuccess() {
+        //given
+        Long id = 1L;
+        TrainerDto trainerDto = new TrainerDto();
+        TrainerEntity trainerEntity = trainerMapper.dtoToEntity(trainerDto);
+        when(trainerMapper.dtoToEntity(trainerDto)).thenReturn(trainerEntity);
+        doNothing().when(trainerService).updateTrainerById(id, trainerEntity);
+
+        //when
+        trainerFacade.updateTrainerById(id, trainerDto);
+
+        //then
+        verify(trainerService).updateTrainerById(id, trainerEntity);
+    }
+
+    @Test
+    public void testUpdateTrainerByIdInvalidPassword() {
+        //given
+        Long id = 1L;
+        TrainerDto trainerDto = new TrainerDto();
+        String password = "illegalPassword";
+        TrainerEntity trainerEntity = trainerMapper.dtoToEntity(trainerDto);
+        when(trainerMapper.dtoToEntity(trainerDto)).thenReturn(trainerEntity);
+        doThrow(new GymIllegalPasswordException(password))
+                .when(trainerService).updateTrainerById(id, trainerEntity);
+
+        //when
+        trainerFacade.updateTrainerById(id, trainerDto);
+
+        //then
+        verify(trainerService).updateTrainerById(id, trainerEntity);
+        GymIllegalPasswordException exception = assertThrows(GymIllegalPasswordException.class,
+                () -> trainerService.updateTrainerById(id, trainerEntity));
+        assertEquals("Illegal password: " + password, exception.getMessage());
 
     }
 
+    @Test
+    public void testUpdateTrainerByIdIllegalId() {
+        //given
+        Long id = 1L;
+        TrainerDto trainerDto = new TrainerDto();
+        TrainerEntity trainerEntity = trainerMapper.dtoToEntity(trainerDto);
+        when(trainerMapper.dtoToEntity(trainerDto)).thenReturn(trainerEntity);
+        doThrow(new GymIllegalIdException("No trainer with id: " + id))
+                .when(trainerService).updateTrainerById(id, trainerEntity);
+
+        //when
+        trainerFacade.updateTrainerById(id, trainerDto);
+
+        //then
+        verify(trainerService).updateTrainerById(id, trainerEntity);
+        GymIllegalIdException exception = assertThrows(GymIllegalIdException.class,
+                () -> trainerService.updateTrainerById(id, trainerEntity));
+        assertEquals("No trainer with id: " + id, exception.getMessage());
+    }
 
 }
