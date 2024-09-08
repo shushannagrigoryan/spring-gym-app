bd460c388815e7fae78cbc8fae89d9a42b995646
Shushanna Grigoryan <shushannagrigoryan7@gmail.com>
2024-09-08 18:57:14 +0400

    Unit tests for trainingDao class.

--- src/main/java/org/example/dao/TrainingDao.java
+++ src/main/java/org/example/dao/TrainingDao.java
@@ -1,19 +1,20 @@
 package org.example.dao;
 
 import java.util.Optional;
-import java.util.OptionalLong;
 import org.example.entity.TrainingEntity;
 import org.example.storage.DataStorage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TrainingDao {
-    @Autowired
     private final DataStorage dataStorage;
 
-    public TrainingDao(DataStorage dataStorage) {
+    private final IdGenerator idGenerator;
+
+
+    public TrainingDao(DataStorage dataStorage, IdGenerator idGenerator) {
         this.dataStorage = dataStorage;
+        this.idGenerator = idGenerator;
     }
 
     /**
@@ -22,24 +23,11 @@ public class TrainingDao {
      * @param trainingEntity {@code TrainingEntity} to be added to storage
      */
     public void createTraining(TrainingEntity trainingEntity) {
-        Long id = generateId();
+        Long id = idGenerator.generateId("Training");
         trainingEntity.setTrainingId(id);
         dataStorage.getTrainingStorage().put(id, trainingEntity);
     }
 
-    /**
-     * Generates a unique id for the training entity.
-     */
-    public Long generateId() {
-        OptionalLong lastId = dataStorage.getTrainingStorage().values().stream()
-                .mapToLong(TrainingEntity::getTrainingId)
-                .max();
-        if (lastId.isPresent()) {
-            return lastId.getAsLong() + 1;
-        } else {
-            return 0L;
-        }
-    }
 
     /**
      * Gets training by id.
--- src/test/java/daotest/TrainingDaoTest.java
+++ src/test/java/daotest/TrainingDaoTest.java
@@ -1,23 +1,25 @@
 package daotest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import org.example.dao.IdGenerator;
 import org.example.dao.TrainingDao;
 import org.example.entity.TrainingEntity;
 import org.example.storage.DataStorage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,60 +27,61 @@ public class TrainingDaoTest {
     @Mock
     private DataStorage dataStorage;
 
+    @Spy
+    private Map<Long, TrainingEntity> trainingEntityMap = new HashMap<>();
     @Mock
-    private Map<Long, TrainingEntity> trainingEntityMap;
+    private IdGenerator idGenerator;
 
     @InjectMocks
     private TrainingDao trainingDao;
 
+    @BeforeEach
+    void setUp() {
+        lenient().when(dataStorage.getTrainingStorage()).thenReturn(trainingEntityMap);
+    }
+
+    @Test
+    public void testCreateTraining() {
+        //given
+        Long id = 1L;
+        TrainingEntity trainingEntity = new TrainingEntity();
+        when(idGenerator.generateId("Training")).thenReturn(id);
+
+        //when
+        trainingDao.createTraining(trainingEntity);
+
+        //then
+        verify(trainingEntityMap, times(1)).put(id, trainingEntity);
+        assertTrue(trainingEntityMap.containsKey(id));
+    }
+
     @Test
     public void testGetTrainingByIdSuccess() {
+        //given
         Long id = 1L;
         TrainingEntity trainingEntity = new TrainingEntity();
         trainingEntity.setTrainingId(id);
-
-        when(dataStorage.getTrainingStorage()).thenReturn(trainingEntityMap);
         when(trainingEntityMap.get(id)).thenReturn(trainingEntity);
 
+        //when
+        Optional<TrainingEntity> result = trainingDao.getTrainingById(id);
 
-        Optional<TrainingEntity> actualTraining = trainingDao.getTrainingById(id);
-        assertTrue(actualTraining.isPresent());
+        //then
+        assertTrue(result.isPresent());
         verify(dataStorage.getTrainingStorage(), times(1)).get(id);
     }
 
     @Test
     void testGetTrainingByIdFailure() {
-        when(dataStorage.getTrainingStorage()).thenReturn(trainingEntityMap);
-        when(trainingEntityMap.get(1L)).thenReturn(null);
+        //given
+        Long id = 1L;
+        when(trainingEntityMap.get(id)).thenReturn(null);
 
+        //when
+        Optional<TrainingEntity> result = trainingDao.getTrainingById(id);
 
-        Optional<TrainingEntity> result = trainingDao.getTrainingById(1L);
-
+        //then
+        verify(dataStorage.getTrainingStorage(), times(1)).get(id);
         assertFalse(result.isPresent());
-    }
-
-    @Test
-    public void generateIdEmptyMap() {
-        when(dataStorage.getTrainingStorage()).thenReturn(trainingEntityMap);
-        when(trainingEntityMap.values()).thenReturn(Collections.emptyList());
-
-
-        Long generatedId = trainingDao.generateId();
-        assertEquals(0L, generatedId);
-    }
-
-    @Test
-    void testGenerateIdWhenStorageHasElements() {
-        TrainingEntity trainingEntity = new TrainingEntity();
-        trainingEntity.setTrainingId(1L);
-
-        Map<Long, TrainingEntity> map = new HashMap<>();
-        map.put(1L, trainingEntity);
-        when(dataStorage.getTrainingStorage()).thenReturn(trainingEntityMap);
-        when(trainingEntityMap.values()).thenReturn(map.values());
-
-        Long generatedId = trainingDao.generateId();
-
-        assertEquals(2L, generatedId);
     }
 }
