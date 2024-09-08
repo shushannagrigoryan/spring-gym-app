361d87dcee5b5003e65528121f617ed96902be1e
Shushanna Grigoryan <shushannagrigoryan7@gmail.com>
2024-09-08 18:56:43 +0400

    Unit tests for traineeDao class.

--- src/test/java/daotest/TraineeDaoTest.java
+++ src/test/java/daotest/TraineeDaoTest.java
@@ -1,24 +1,28 @@
 package daotest;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Collections;
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import org.example.dao.IdGenerator;
 import org.example.dao.TraineeDao;
 import org.example.entity.TraineeEntity;
 import org.example.exceptions.GymIllegalIdException;
 import org.example.storage.DataStorage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,11 +30,40 @@ public class TraineeDaoTest {
     @Mock
     private DataStorage dataStorage;
 
+    @Spy
+    private Map<Long, TraineeEntity> traineeEntityMap = new HashMap<>();
+    @Spy
+    private Map<String, TraineeEntity> traineeEntityMapUsernameKey = new HashMap<>();
     @Mock
-    private Map<Long, TraineeEntity> traineeEntityMap;
+    private IdGenerator idGenerator;
 
     @InjectMocks
     private TraineeDao traineeDao;
+
+    @BeforeEach
+    void setUp() {
+        lenient().when(dataStorage.getTraineeStorage()).thenReturn(traineeEntityMap);
+        lenient().when(dataStorage.getTraineeStorageUsernameKey()).thenReturn(traineeEntityMapUsernameKey);
+    }
+
+    @Test
+    public void testCreateTrainee() {
+        //given
+        Long id = 1L;
+        String username = "A.B";
+        TraineeEntity traineeEntity = new TraineeEntity();
+        traineeEntity.setUsername(username);
+        when(idGenerator.generateId("Trainee")).thenReturn(id);
+
+        //when
+        traineeDao.createTrainee(traineeEntity);
+
+        //then
+        verify(traineeEntityMap, times(1)).put(id, traineeEntity);
+        verify(traineeEntityMapUsernameKey, times(1)).put(username, traineeEntity);
+        assertTrue(traineeEntityMap.containsKey(id));
+        assertTrue(traineeEntityMapUsernameKey.containsKey(username));
+    }
 
     @Test
     public void testGetTraineeByIdSuccess() {
@@ -38,102 +71,115 @@ public class TraineeDaoTest {
         Long id = 1L;
         TraineeEntity traineeEntity = new TraineeEntity();
         traineeEntity.setUserId(id);
-        when(dataStorage.getTraineeStorage()).thenReturn(traineeEntityMap);
         when(traineeEntityMap.get(id)).thenReturn(traineeEntity);
 
         //when
-        Optional<TraineeEntity> actualTrainee = traineeDao.getTraineeById(id);
+        Optional<TraineeEntity> result = traineeDao.getTraineeById(id);
 
         //then
-        assertTrue(actualTrainee.isPresent());
+        assertTrue(result.isPresent());
         verify(dataStorage.getTraineeStorage(), times(1)).get(id);
     }
 
     @Test
     void testGetTraineeByIdFailure() {
-        when(dataStorage.getTraineeStorage()).thenReturn(traineeEntityMap);
-        when(traineeEntityMap.get(1L)).thenReturn(null);
+        //given
+        Long id = 1L;
+        when(traineeEntityMap.get(id)).thenReturn(null);
 
-        Optional<TraineeEntity> result = traineeDao.getTraineeById(1L);
+        //when
+        Optional<TraineeEntity> result = traineeDao.getTraineeById(id);
 
+        //then
+        verify(dataStorage.getTraineeStorage(), times(1)).get(id);
         assertFalse(result.isPresent());
     }
 
     @Test
-    public void generateIdEmptyMap() {
-        when(dataStorage.getTraineeStorage()).thenReturn(Collections.emptyMap());
-
-        Long generatedId = traineeDao.generateId();
-        assertEquals(0L, generatedId);
-    }
-
-    @Test
-    void testDeleteTraineeById() {
-
-        when(dataStorage.getTraineeStorage()).thenReturn(traineeEntityMap);
+    void testDeleteTraineeByIdSuccess() {
+        //given
         when(traineeEntityMap.containsKey(1L)).thenReturn(true);
 
+        //when
         traineeDao.deleteTraineeById(1L);
 
-        verify(dataStorage.getTraineeStorage()).remove(1L);
+        //then
+        verify(dataStorage.getTraineeStorage(), times(1)).remove(1L);
     }
 
     @Test
     void testDeleteTraineeByIdThrowsException() {
-        when(dataStorage.getTraineeStorage()).thenReturn(traineeEntityMap);
+        //given
         when(traineeEntityMap.containsKey(1L)).thenReturn(false);
+
+        //then
         assertThatThrownBy(() -> traineeDao.deleteTraineeById(1L))
                 .isInstanceOf(GymIllegalIdException.class)
                 .hasMessageContaining("No trainee with id: " + 1L);
     }
 
-    /*
-
-   @Test
-    void testGetTraineeByUsernameNotFound() {
-        when(dataStorage.getTraineeStorage().values()).thenReturn(Collections.emptyList());
-
-        Optional<TraineeEntity> result = traineeDao.getTraineeByUsername("JackSmith");
-
-        assertFalse(result.isPresent());
-    }
-
-     */
-
-
     @Test
-    void testUpdateTraineeById() {
-        TraineeEntity traineeEntity = new TraineeEntity();
-        when(dataStorage.getTraineeStorage()).thenReturn(traineeEntityMap);
-        when(traineeEntityMap.containsKey(1L)).thenReturn(true);
+    void testUpdateTraineeByIdSuccess() {
+        //given
+        Long id = 1L;
+        TraineeEntity traineeEntity = new TraineeEntity("A", "B",
+                "myPassword", LocalDate.now(), "myAddress");
+        traineeEntity.setUserId(id);
 
-        traineeDao.updateTraineeById(1L, traineeEntity);
+        when(traineeEntityMap.containsKey(id)).thenReturn(true);
 
-        verify(dataStorage.getTraineeStorage()).put(1L, traineeEntity);
+        //when
+        traineeDao.updateTraineeById(id, traineeEntity);
+
+        //then
+        verify(traineeEntityMap).put(id, traineeEntity);
+        verify(traineeEntityMapUsernameKey).put(traineeEntity.getUsername(), traineeEntity);
+        assertTrue(traineeEntityMap.containsKey(id));
+        assertTrue(traineeEntityMapUsernameKey.containsKey(traineeEntity.getUsername()));
     }
 
     @Test
     void testUpdateTraineeByIdThrowsException() {
-        TraineeEntity traineeEntity = new TraineeEntity();
-        when(dataStorage.getTraineeStorage()).thenReturn(traineeEntityMap);
-        when(traineeEntityMap.containsKey(1L)).thenReturn(false);
+        //given
+        Long id = 1L;
+        TraineeEntity traineeEntity = new TraineeEntity("A", "B",
+                "myPassword", LocalDate.now(), "myAddress");
+        traineeEntity.setUserId(id);
+        when(traineeEntityMap.containsKey(id)).thenReturn(false);
 
-        assertThatThrownBy(() -> traineeDao.updateTraineeById(1L, traineeEntity))
+        //then
+        assertThatThrownBy(() -> traineeDao.updateTraineeById(id, traineeEntity))
                 .isInstanceOf(GymIllegalIdException.class)
-                .hasMessageContaining("No trainee with id: " + 1L);
+                .hasMessageContaining("No trainee with id: " + id);
     }
 
-    //    @Test
-    //    void testGenerateIdWhenStorageHasElements() {
-    //        TraineeEntity traineeEntity = new TraineeEntity();
-    //        traineeEntity.setUserId(1L);
-    //
-    //        Map<Long, TraineeEntity> map = new HashMap<>();
-    //        map.put(1L, traineeEntity);
-    //        when(dataStorage.getTraineeStorage().values()).thenReturn(map.values());
-    //
-    //        Long generatedId = traineeDao.generateId();
-    //
-    //        assertEquals(2L, generatedId);
-    //    }
+
+
+    @Test
+    void testGetTraineeByUsernameNotFound() {
+        //given
+        String username = "Jack.Smith";
+        when(traineeEntityMapUsernameKey.get(username)).thenReturn(null);
+
+        //when
+        Optional<TraineeEntity> result = traineeDao.getTraineeByUsername(username);
+
+        //then
+        assertFalse(result.isPresent());
+    }
+
+    @Test
+    void testGetTraineeByUsernameFound() {
+        //given
+        String username = "Jack.Smith";
+        TraineeEntity traineeEntity = new TraineeEntity();
+        when(traineeEntityMapUsernameKey.get(username)).thenReturn(traineeEntity);
+
+        //when
+        Optional<TraineeEntity> result = traineeDao.getTraineeByUsername(username);
+
+        //then
+        assertTrue(result.isPresent());
+    }
+
 }
