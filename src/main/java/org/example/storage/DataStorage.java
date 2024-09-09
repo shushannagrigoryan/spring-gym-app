ad38e7af40d165f6c39bfb88bc6dd7cc0287ebf4
Shushanna Grigoryan <shushannagrigoryan7@gmail.com>
2024-09-09 12:39:43 +0400

    Created 3 separate methods for reading data from file instead of writing all the  logic in storage init method.

--- src/main/java/org/example/storage/DataStorage.java
+++ src/main/java/org/example/storage/DataStorage.java
@@ -40,48 +40,64 @@ public class DataStorage {
      * Initializes the storage bean from external files.
      */
     @PostConstruct
-    private void init() {
+    public void init() {
+        System.out.println(1);
         ObjectMapper objectMapper = new ObjectMapper();
+        System.out.println(2);
         objectMapper.findAndRegisterModules();
+        System.out.println(3);
         try {
+            System.out.println(traineeStorageFile);
             File traineeFile = new File(traineeStorageFile);
             File trainerFile = new File(trainerStorageFile);
             File trainingFile = new File(trainingStorageFile);
 
+            System.out.println(4);
             if (traineeFile.length() > 0) {
-                log.debug("Initializing trainee storage map from file data.");
-                traineeStorage
-                        .putAll(objectMapper.readValue(traineeFile, new TypeReference<Map<Long, TraineeEntity>>() {
-                        }));
-
-                // populate a different map using username as key
-                for (Map.Entry<Long, TraineeEntity> entry : traineeStorage.entrySet()) {
-                    TraineeEntity trainee = entry.getValue();
-                    traineeStorageUsernameKey.put(trainee.getUsername(), trainee);
-                }
-
+                initTrainee(objectMapper, traineeFile);
             }
             if (trainerFile.length() > 0) {
-                log.debug("Initializing trainer storage map from file data.");
-                trainerStorage
-                        .putAll(objectMapper.readValue(trainerFile, new TypeReference<Map<Long, TrainerEntity>>() {
-                        }));
-
-                // populate a different map using username as key
-                for (Map.Entry<Long, TrainerEntity> entry : trainerStorage.entrySet()) {
-                    TrainerEntity trainer = entry.getValue();
-                    trainerStorageUsernameKey.put(trainer.getUsername(), trainer);
-                }
+                initTrainer(objectMapper, trainerFile);
             }
             if (trainingFile.length() > 0) {
-                log.debug("Initializing training storage map from file data.");
-                trainingStorage.putAll(objectMapper.readValue(trainingFile,
-                        new TypeReference<Map<Long, TrainingEntity>>() {
-                        }));
+                initTraining(objectMapper, trainingFile);
             }
         } catch (IOException e) {
             log.debug("Failed to read  files: {}, {}, {}. Exception: {}",
                     traineeStorageFile, trainerStorageFile, trainingStorageFile, e.getMessage());
         }
+    }
+
+    private void initTrainee(ObjectMapper objectMapper, File traineeFile) throws IOException {
+        log.debug("Initializing trainee storage map from file data.");
+        traineeStorage
+                .putAll(objectMapper.readValue(traineeFile, new TypeReference<Map<Long, TraineeEntity>>() {
+                }));
+
+        // populate a different map using username as key
+        for (Map.Entry<Long, TraineeEntity> entry : traineeStorage.entrySet()) {
+            TraineeEntity trainee = entry.getValue();
+            traineeStorageUsernameKey.put(trainee.getUsername(), trainee);
+        }
+    }
+
+    private void initTrainer(ObjectMapper objectMapper, File trainerFile) throws IOException {
+        log.debug("Initializing trainer storage map from file data.");
+        trainerStorage
+                .putAll(objectMapper.readValue(trainerFile, new TypeReference<Map<Long, TrainerEntity>>() {
+                }));
+
+        // populate a different map using username as key
+        for (Map.Entry<Long, TrainerEntity> entry : trainerStorage.entrySet()) {
+            TrainerEntity trainer = entry.getValue();
+            trainerStorageUsernameKey.put(trainer.getUsername(), trainer);
+        }
+    }
+
+    private void initTraining(ObjectMapper objectMapper, File trainingFile) throws IOException {
+        log.debug("Initializing training storage map from file data.");
+        trainingStorage.putAll(objectMapper.readValue(trainingFile,
+                new TypeReference<Map<Long, TrainingEntity>>() {
+                }));
     }
 }
