596a9714e51842de5eedda2f531fb74299f69460
Shushanna Grigoryan <shushannagrigoryan7@gmail.com>
2024-09-17 23:26:16 +0400

    Moved password generation to a separate class.

---
+++ src/main/java/org/example/password/PasswordGeneration.java
@@ -0,0 +1,23 @@
+package org.example.password;
+
+import java.security.SecureRandom;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class PasswordGeneration {
+    private static final SecureRandom secureRandom = new SecureRandom();
+    private static final int startChar = 32;
+    private static final int endChar = 127;
+
+    /** Generating random 10 length password. */
+    public String generatePassword() {
+        log.debug("Generating a random password.");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            stringBuilder.append((char) secureRandom.nextInt(startChar, endChar));
+        }
+        return stringBuilder.toString();
+    }
+}
