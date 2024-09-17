cb58131216f6c7051b11b04f25f5d34ba245956b
Shushanna Grigoryan <shushannagrigoryan7@gmail.com>
2024-09-17 22:31:28 +0400

    TraineeService getTraineeByUsername method.

--- src/main/java/org/example/services/TraineeService.java
+++ src/main/java/org/example/services/TraineeService.java
@@ -3,35 +3,38 @@ package org.example.services;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.example.dao.TraineeDao;
-import org.example.dao.UserDao;
 import org.example.dto.TraineeDto;
 import org.example.entity.TraineeEntity;
 import org.example.exceptions.GymIllegalIdException;
 import org.example.mapper.TraineeMapper;
-import org.example.storage.SaveDataToFile;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class TraineeService {
-    private TraineeDao traineeDao;
-    private UserDao userDao;
-    private SaveDataToFile saveDataToFile;
-    private TraineeMapper traineeMapper;
+    private final TraineeDao traineeDao;
+    private final TraineeMapper traineeMapper;
 
-    /**
-     * Setting the dependencies for the TraineeService bean.
-     */
-    @Autowired
-    public void setDependencies(UserDao userDao, TraineeDao traineeDao,
-                                SaveDataToFile saveDataToFile,
-                                TraineeMapper traineeMapper) {
-        this.userDao = userDao;
+    public TraineeService(TraineeDao traineeDao, TraineeMapper traineeMapper){
         this.traineeDao = traineeDao;
-        this.saveDataToFile = saveDataToFile;
         this.traineeMapper = traineeMapper;
     }
+    //private UserDao userDao;
+
+    //private TraineeMapper traineeMapper;
+
+    //    /**
+    //     * Setting the dependencies for the TraineeService bean.
+    //     */
+    //    @Autowired
+    //    public void setDependencies(UserDao userDao, TraineeDao traineeDao,
+    //                                SaveDataToFile saveDataToFile,
+    //                                TraineeMapper traineeMapper) {
+    //        this.userDao = userDao;
+    //        this.traineeDao = traineeDao;
+    //        //this.saveDataToFile = saveDataToFile;
+    //        this.traineeMapper = traineeMapper;
+    //    }
 
     /**
      * Creates a new trainee in the service layer.
@@ -40,12 +43,14 @@ public class TraineeService {
      */
     public void createTrainee(TraineeEntity traineeEntity) {
         log.debug("Creating trainee: {}", traineeEntity);
-        String username = userDao.generateUsername(traineeEntity.getFirstName(), traineeEntity.getLastName());
-        traineeEntity.setUsername(username);
-        traineeEntity.setPassword(userDao.generatePassword());
+        //String username = userDao.generateUsername(traineeEntity.getFirstName(), traineeEntity.getLastName());
+        //traineeEntity.setUsername(username);
+        //traineeEntity.setPassword(userDao.generatePassword());
+
         traineeDao.createTrainee(traineeEntity);
-        saveDataToFile.writeMapToFile("Trainee");
         log.debug("Successfully created trainee: {}", traineeEntity);
+
+
     }
 
     /**
@@ -57,13 +62,13 @@ public class TraineeService {
      */
     public TraineeDto getTraineeByUsername(String username) {
         log.debug("Retrieving trainee by username: {}", username);
-        Optional<TraineeEntity> trainee = traineeDao.getTraineeByUsername(username);
-        if (trainee.isEmpty()) {
+        TraineeEntity trainee = traineeDao.getTraineeByUsername(username);
+        if (trainee == null) {
             log.debug("No trainee with the username: {}", username);
             return null;
         }
         log.debug("Successfully retrieved trainee by username: {}", username);
-        return traineeMapper.entityToDto(trainee.get());
+        return traineeMapper.entityToDto(trainee);
     }
 
     /**
@@ -75,63 +80,65 @@ public class TraineeService {
      */
     public TraineeDto getTraineeById(Long id) {
         log.debug("Retrieving trainee by id: {}", id);
-        Optional<TraineeEntity> trainee = traineeDao.getTraineeById(id);
-        if (trainee.isEmpty()) {
+        TraineeEntity trainee = traineeDao.getTraineeById(id);
+        if (trainee == null) {
             throw new GymIllegalIdException(String.format("No trainee with id: %d", id));
         }
+        System.out.println("retrieved trainee = " + trainee);
         log.debug("Successfully retrieved trainee by id: {}", id);
-        return traineeMapper.entityToDto(trainee.get());
+        return traineeMapper.entityToDto(trainee);
+
     }
-
-    /**
-     * Deletes a trainee by id in the service layer.
-     * If there is no trainee with the given id throws an {@code GymIllegalIdException}.
-     *
-     * @param id the id of the trainee
-     */
-    public void deleteTraineeById(Long id) {
-        log.debug("Deleting trainee by id: {}", id);
-        traineeDao.deleteTraineeById(id);
-        saveDataToFile.writeMapToFile("Trainee");
-        log.debug("Successfully deleted trainee by id: {}", id);
-    }
-
-    /**
-     * Updates trainee by id.
-     *
-     * @param id            id of the trainee
-     * @param traineeEntity {@code TraineeEntity} to update with
-     */
-    public void updateTraineeById(Long id, TraineeEntity traineeEntity) {
-        log.debug("Updating trainee by id: {}", id);
-        Optional<TraineeEntity> trainee = traineeDao.getTraineeById(id);
-
-        if (trainee.isEmpty()) {
-            log.debug("No trainee with id: {}", id);
-            throw new GymIllegalIdException(String.format("No trainee with id: %d", id));
-        }
-
-        TraineeEntity traineeToUpdate = trainee.get();
-
-        String updatedFirstName = traineeEntity.getFirstName();
-        String updatedLastName = traineeEntity.getLastName();
-        String firstName = traineeToUpdate.getFirstName();
-        String lastName = traineeToUpdate.getLastName();
-
-        if (!((firstName.equals(updatedFirstName)) && (lastName.equals(updatedLastName)))) {
-            String username = userDao
-                    .generateUsername(updatedFirstName, updatedLastName);
-            traineeToUpdate.setUsername(username);
-        }
-
-        traineeToUpdate.setFirstName(updatedFirstName);
-        traineeToUpdate.setLastName(updatedLastName);
-        traineeToUpdate.setDateOfBirth(traineeEntity.getDateOfBirth());
-        traineeToUpdate.setAddress(traineeEntity.getAddress());
-        traineeToUpdate.setActive(traineeEntity.isActive());
-
-        traineeDao.updateTraineeById(id, traineeToUpdate);
-        log.debug("Successfully updated trainee with id: {}", id);
-        saveDataToFile.writeMapToFile("Trainee");
-    }
+    //
+    //    /**
+    //     * Deletes a trainee by id in the service layer.
+    //     * If there is no trainee with the given id throws an {@code GymIllegalIdException}.
+    //     *
+    //     * @param id the id of the trainee
+    //     */
+    //    public void deleteTraineeById(Long id) {
+    //        log.debug("Deleting trainee by id: {}", id);
+    //        traineeDao.deleteTraineeById(id);
+    //        saveDataToFile.writeMapToFile("Trainee");
+    //        log.debug("Successfully deleted trainee by id: {}", id);
+    //    }
+    //
+    //    /**
+    //     * Updates trainee by id.
+    //     *
+    //     * @param id            id of the trainee
+    //     * @param traineeEntity {@code TraineeEntity} to update with
+    //     */
+    //    public void updateTraineeById(Long id, TraineeEntity traineeEntity) {
+    //        log.debug("Updating trainee by id: {}", id);
+    //        Optional<TraineeEntity> trainee = traineeDao.getTraineeById(id);
+    //
+    //        if (trainee.isEmpty()) {
+    //            log.debug("No trainee with id: {}", id);
+    //            throw new GymIllegalIdException(String.format("No trainee with id: %d", id));
+    //        }
+    //
+    //        TraineeEntity traineeToUpdate = trainee.get();
+    //
+    //        String updatedFirstName = traineeEntity.getFirstName();
+    //        String updatedLastName = traineeEntity.getLastName();
+    //        String firstName = traineeToUpdate.getFirstName();
+    //        String lastName = traineeToUpdate.getLastName();
+    //
+    //        if (!((firstName.equals(updatedFirstName)) && (lastName.equals(updatedLastName)))) {
+    //            String username = userDao
+    //                    .generateUsername(updatedFirstName, updatedLastName);
+    //            traineeToUpdate.setUsername(username);
+    //        }
+    //
+    //        traineeToUpdate.setFirstName(updatedFirstName);
+    //        traineeToUpdate.setLastName(updatedLastName);
+    //        traineeToUpdate.setDateOfBirth(traineeEntity.getDateOfBirth());
+    //        traineeToUpdate.setAddress(traineeEntity.getAddress());
+    //        traineeToUpdate.setActive(traineeEntity.isActive());
+    //
+    //        traineeDao.updateTraineeById(id, traineeToUpdate);
+    //        log.debug("Successfully updated trainee with id: {}", id);
+    //        saveDataToFile.writeMapToFile("Trainee");
+    //    }
 }
