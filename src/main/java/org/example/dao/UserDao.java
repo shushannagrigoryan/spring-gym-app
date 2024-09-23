dbbe1129c46df0e9b0047c831e4c98969dc62358
Shushanna Grigoryan <shushannagrigoryan7@gmail.com>
2024-09-23 17:08:03 +0400

    Removed redundant transaction management from UserDao.

--- src/main/java/org/example/dao/UserDao.java
+++ src/main/java/org/example/dao/UserDao.java
@@ -44,17 +44,12 @@ public class UserDao {
      */
     public List<String> getAllUsernames() {
         List<String> allUsernames = null;
-        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
             String hql = "SELECT t.username FROM UserEntity t";
             Query<String> query = session.createQuery(hql, String.class);
             allUsernames = query.getResultList();
-            transaction.commit();
         } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
+            log.debug("hibernate exception");
         }
         return allUsernames;
     }
@@ -69,19 +64,14 @@ public class UserDao {
     public Optional<UserEntity> getUserByUsername(String username) {
         log.debug("Getting user with username: {}", username);
 
-        Optional<UserEntity> user = null;
-        Transaction transaction = null;
+        Optional<UserEntity> user;
         try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
             String hql = "FROM UserEntity u WHERE u.username = :username";
             Query<UserEntity> query = session.createQuery(hql, UserEntity.class);
             query.setParameter("username", username);
             user = Optional.ofNullable(query.uniqueResult());
 
         } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
             log.debug("hibernate exception");
             throw new GymDataAccessException(
                     String.format("Failed to retrieve user with username: %s", username));
@@ -89,69 +79,4 @@ public class UserDao {
         return user;
     }
 
-    //    /**
-    //     * Generates username for the user (firstName.lastName)
-    //     * Adds suffix if the username is taken either by a trainee or a trainer.
-    //     *
-    //     * @param firstName firstName of the user
-    //     * @param lastName lastName of the user
-    //     * @return the username
-    //     */
-    //    public String generateUsername(String firstName, String lastName) {
-    //        log.debug("Generating username for firstName: {}, lastName: {}", firstName, lastName);
-    //
-    //        String username = firstName.concat(".").concat(lastName);
-    //
-    //        TraineeDto trainee = traineeService.getTraineeByUsername(username);
-    //        TrainerDto trainer = trainerService.getTrainerByUsername(username);
-    //
-    //        if ((trainee != null) || (trainer != null)) {
-    //            log.debug("Username taken.");
-    //            return username + getSuffix(username);
-    //        }
-    //        return username;
-    //    }
-    //
-    //
-    //    /**
-    //     * Generates suffix for the given username which is not present both in trainee and trainer maps.
-    //     *
-    //     * @param username username for which suffix is generated
-    //     * @return the suffix
-    //     */
-    //    private Long getSuffix(String username) {
-    //        log.debug("Generating suffix for username: {}", username);
-    //
-    //        Long maxTraineeSuffix = dataStorage.getTraineeStorage().values()
-    //                .stream()
-    //                .map(TraineeEntity::getUsername)
-    //                .filter(u -> u.startsWith(username))
-    //                .map(key -> key.substring(username.length()))
-    //                .filter(s -> !s.isEmpty()).map(Long::valueOf)
-    //                .max(Long::compareTo).orElse(-1L);
-    //
-    //        Long maxTrainerSuffix = dataStorage.getTrainerStorage().values()
-    //                .stream()
-    //                .map(TrainerEntity::getUsername)
-    //                .filter(u -> u.startsWith(username))
-    //                .map(key -> key.substring(username.length()))
-    //                .filter(s -> !s.isEmpty())
-    //                .map(Long::valueOf)
-    //                .max(Long::compareTo).orElse(-1L);
-    //
-    //        long suffix = maxTraineeSuffix > maxTrainerSuffix ? maxTraineeSuffix : maxTrainerSuffix;
-    //
-    //        return suffix + 1;
-    //    }
-    //
-    //    /** Generating random 10 length password. */
-    //    public String generatePassword() {
-    //        log.debug("Generating a random password.");
-    //        StringBuilder stringBuilder = new StringBuilder();
-    //        SecureRandom secureRandom = new SecureRandom();
-    //        for (int i = 0; i < 10; i++) {
-    //            stringBuilder.append((char) secureRandom.nextInt(32, 127));
-    //        }
-    //        return stringBuilder.toString();
-    //    }
 }
