88a548333bd206aeb414d94ed3f3edc2386fd8f6
Koryun Grigoryan <64692259+koryun23@users.noreply.github.com>
2024-09-30 16:16:33 +0400

    Adding review comments regarding the GymIllegalIdException class

--- src/main/java/org/example/exceptions/GymIllegalIdException.java
+++ src/main/java/org/example/exceptions/GymIllegalIdException.java
@@ -1,5 +1,10 @@
 package org.example.exceptions;
 
+// Every time this exception gets thrown, the same message should be
+// copied multiple times as an argument, which may be inconvenient.
+// I would pass the id to the constructor instead of the message,
+// and call the super constructor with a custom message.
+
 public class GymIllegalIdException extends GymCustomRuntimeException {
     public GymIllegalIdException(String message) {
         super(message);
