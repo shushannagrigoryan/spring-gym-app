ac7465486aab4a039188cedfce92e549430ce103
Shushanna Grigoryan <shushannagrigoryan7@gmail.com>
2024-10-20 17:47:25 +0400

    Modified logging to log tranceId only when request is made.

--- src/main/java/org/example/config/MySwaggerConfig.java
+++ src/main/java/org/example/config/MySwaggerConfig.java
@@ -7,10 +7,12 @@ import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @Slf4j
-public class SwaggerConfig {
+@Import({org.springdoc.webmvc.ui.SwaggerConfig.class})
+public class MySwaggerConfig {
     /**
      * config.
      */
