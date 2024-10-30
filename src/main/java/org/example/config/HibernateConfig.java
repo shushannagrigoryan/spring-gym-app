c4a69039f8fa85e2274ff614dfd9ba2f8b2d2bb9
Shushanna Grigoryan <shushannagrigoryan7@gmail.com>
2024-10-30 21:48:04 +0400

    Changed get trainee/trainer trainings by filter methods , created spring data jpa specifications.

--- src/main/java/org/example/config/HibernateConfig.java
+++ src/main/java/org/example/config/HibernateConfig.java
@@ -1,74 +1,74 @@
-package org.example.config;
-
-import java.util.Properties;
-import javax.sql.DataSource;
-import org.example.entity.TraineeEntity;
-import org.example.entity.TrainerEntity;
-import org.example.entity.TrainingEntity;
-import org.example.entity.TrainingTypeEntity;
-import org.example.entity.UserEntity;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-
-@Configuration
-@PropertySource("classpath:application.properties")
-@ComponentScan(basePackages = "org.example")
-@EnableAspectJAutoProxy
-@EnableTransactionManagement
-public class HibernateConfig {
-    /**
-     * Data Source.
-     */
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/gymdb");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres");
-        return dataSource;
-    }
-
-    /**
-     * SessionFactory.
-     */
-    @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("org.example.entity");
-
-        Properties hibernateProperties = new Properties();
-        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        hibernateProperties.put("hibernate.show_sql", "true");
-        hibernateProperties.put("hibernate.hbm2ddl.auto", "update");
-        sessionFactory.setHibernateProperties(hibernateProperties);
-
-        sessionFactory.setAnnotatedClasses(
-                TrainerEntity.class,
-                TraineeEntity.class,
-                TrainingEntity.class,
-                TrainingTypeEntity.class,
-                UserEntity.class
-        );
-
-        return sessionFactory;
-    }
-
-    //    /**
-    //     * Hibernate transaction manager.
-    //     */
-    //    @Bean
-    //    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-    //        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-    //        transactionManager.setSessionFactory(sessionFactory);
-    //        return transactionManager;
-    //    }
-}
+//package org.example.config;
+//
+//import java.util.Properties;
+//import javax.sql.DataSource;
+//import org.example.entity.TraineeEntity;
+//import org.example.entity.TrainerEntity;
+//import org.example.entity.TrainingEntity;
+//import org.example.entity.TrainingTypeEntity;
+//import org.example.entity.UserEntity;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.EnableAspectJAutoProxy;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+//
+//@Configuration
+//@PropertySource("classpath:application.properties")
+//@ComponentScan(basePackages = "org.example")
+//@EnableAspectJAutoProxy
+//@EnableTransactionManagement
+//public class HibernateConfig {
+//    /**
+//     * Data Source.
+//     */
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.postgresql.Driver");
+//        dataSource.setUrl("jdbc:postgresql://localhost:5432/gymdb");
+//        dataSource.setUsername("postgres");
+//        dataSource.setPassword("postgres");
+//        return dataSource;
+//    }
+//
+//    /**
+//     * SessionFactory.
+//     */
+//    @Bean
+//    public LocalSessionFactoryBean sessionFactory() {
+//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+//        sessionFactory.setDataSource(dataSource());
+//        sessionFactory.setPackagesToScan("org.example.entity");
+//
+//        Properties hibernateProperties = new Properties();
+//        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+//        hibernateProperties.put("hibernate.show_sql", "true");
+//        hibernateProperties.put("hibernate.hbm2ddl.auto", "update");
+//        sessionFactory.setHibernateProperties(hibernateProperties);
+//
+//        sessionFactory.setAnnotatedClasses(
+//                TrainerEntity.class,
+//                TraineeEntity.class,
+//                TrainingEntity.class,
+//                TrainingTypeEntity.class,
+//                UserEntity.class
+//        );
+//
+//        return sessionFactory;
+//    }
+//
+//    //    /**
+//    //     * Hibernate transaction manager.
+//    //     */
+//    //    @Bean
+//    //    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+//    //        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+//    //        transactionManager.setSessionFactory(sessionFactory);
+//    //        return transactionManager;
+//    //    }
+//}
